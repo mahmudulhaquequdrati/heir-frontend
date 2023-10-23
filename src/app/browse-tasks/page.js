5e0e849578928a1fa29f2a56c44e47e609fd108d
Rakib Hossen <rakibhossen040@gmail.com>
2023-10-23 22:09:25 +0600

    Changes responsive layout in tasks

--- src/app/browse-tasks/page.js
+++ src/app/browse-tasks/page.js
@@ -135,7 +135,7 @@ const page = () => {
           </div>
         </div>
         <div className="">
-          <div className="w-full grid grid-cols-1 sm:grid-cols-2  md:grid-cols-2 lg:grid-cols-2 xl:grid-cols-3 gap-7">
+          <div className="w-full grid grid-cols-1 lg:grid-cols-2 xl:grid-cols-3 gap-7">
             {tasks?.map((task) => (
               <Card task={task} />
             ))}
