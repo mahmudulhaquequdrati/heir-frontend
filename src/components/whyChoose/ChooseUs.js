95e5c1c9877f29b4656f0db84b9e9dd9eb0c2989
mahmudulhaquequdrati <freelancermahmudqudrati@gmail.com>
2023-11-07 22:34:34 +0600

    home page updated

--- src/components/whyChoose/ChooseUs.js
+++ src/components/whyChoose/ChooseUs.js
@@ -5,19 +5,19 @@ import React from "react";
 export default function ChooseUs() {
   return (
     <div className="container mx-auto">
-      <div className="flex items-center justify-center self-center  w-[400px] max-w-full flex-col mt-24 max-md:mt-10 mx-auto">
-        <div className="items-center self-stretch flex flex-col px-5">
-          <div className="text-cyan-900 text-4xl font-semibold tracking-wider self-stretch whitespace-nowrap -mr-5">
+      <div className="flex items-center justify-center   w-[400px]  max-w-full flex-col mt-24 max-md:mt-10 mx-auto mb-24">
+        <div className="items-center  flex flex-col px-5">
+          <div className="text-cyan-900 text-4xl font-semibold tracking-wider">
             Why Choose Hier?
           </div>
-          <div className="text-cyan-900 text-center text-base leading-6 self-stretch whitespace-nowrap -mr-5 mt-2">
+          <div className="text-cyan-900 text-center text-base leading-6 mt-4 ">
             Connect with unlimited quality talents to get any help.
           </div>
         </div>
-        <div className="bg-orange-400 self-center flex w-[205px] h-[9px] flex-col mt-4 rounded-[50px]" />
+        <div className="bg-orange-400  flex w-[205px] h-[9px] flex-col mt-4 rounded-[50px]" />
       </div>
-      <div className="self-center w-full max-w-[1203px] mt-16 px-5 max-md:max-w-full max-md:mt-10">
-        <div className="gap-5 flex max-md:flex-col max-md:items-stretch max-md:gap-0">
+      <div className="self-center  w-full max-w-[1203px] mx-auto mt-16 px-5 max-md:max-w-full max-md:mt-10">
+        <div className="gap-5  flex max-md:flex-col max-md:items-stretch max-md:gap-0 mx-auto">
           <div className="flex flex-col items-stretch w-[41%] max-md:w-full max-md:ml-0">
             <img
               loading="lazy"
@@ -25,101 +25,98 @@ export default function ChooseUs() {
               className="aspect-[0.81] object-contain object-center w-full overflow-hidden grow max-md:max-w-full max-md:mt-7"
             />
           </div>
-          <div className="flex flex-col items-stretch w-[59%] ml-5 max-md:w-full max-md:ml-0">
-            <div className="items-start flex grow flex-col max-md:max-w-full max-md:mt-7">
-              <div className="items-start self-stretch flex flex-col w-full max-md:max-w-full">
-                <div className="self-stretch max-md:max-w-full">
-                  <div className="gap-5 flex max-md:flex-col max-md:items-stretch max-md:gap-0">
-                    <div className="flex flex-col items-stretch w-[48%] max-md:w-full max-md:ml-0">
-                      <div className="items-start flex grow flex-col max-md:mt-10">
-                        <div className="text-white text-xl font-semibold leading-8 whitespace-nowrap bg-orange-400 w-[81px] max-w-full pl-7 pr-7 py-2.5 rounded-[50px] self-start max-md:px-5">
-                          01
-                        </div>
-                        <div className="text-cyan-900 text-3xl font-medium leading-[56.16px] self-stretch whitespace-nowrap mt-5">
-                          Vast Network
-                        </div>
+          <div className="flex flex-col  w-[59%] ml-5 max-md:w-full max-md:ml-0">
+            <div className=" flex  flex-col max-md:max-w-full max-md:mt-7">
+              <div className="mb-6">
+                <div className="flex">
+                  <div className="flex flex-col w-[48%] max-md:w-full max-md:ml-0">
+                    <div className=" flex flex-col max-md:mt-10">
+                      <div className="text-white text-xl font-semibold leading-8  bg-orange-400 w-[81px] text-center max-w-full pl-7 pr-7 py-1.5 rounded-[50px]  max-md:px-5">
+                        04
+                      </div>
+                      <div className="text-cyan-900 text-2xl font-medium leading-[36.16px]  mt-5">
+                        Secure Transactions
                       </div>
                     </div>
-                    <div className="flex flex-col items-stretch w-[52%] ml-5 max-md:w-full max-md:ml-0">
-                      <div className="text-cyan-900 text-base leading-8 w-[310px] max-w-[310px] my-auto max-md:mt-10">
-                        With a diverse community of users from different
-                        backgrounds and skills, Hier offers a wide range of
-                        services.
-                      </div>
+                  </div>
+                  <div className="flex flex-col items-stretch w-[52%] ml-5 max-md:w-full max-md:ml-0">
+                    <div className="text-cyan-900 text-base leading-8  my-auto max-md:mt-10">
+                      With a diverse community of users from different
+                      backgrounds and skills, Hier offers a wide range of
+                      services.
                     </div>
                   </div>
                 </div>
-                <div className="bg-black bg-opacity-10 self-stretch w-full h-px mt-5 max-md:max-w-full" />
+                <div className="bg-black bg-opacity-10  w-full h-px mt-5 max-md:max-w-full" />
               </div>
-              <div className="items-start self-stretch flex flex-col w-full mt-6 max-md:max-w-full">
-                <div className="self-stretch max-md:max-w-full">
-                  <div className="gap-5 flex max-md:flex-col max-md:items-stretch max-md:gap-0">
-                    <div className="flex flex-col items-stretch w-[48%] max-md:w-full max-md:ml-0">
-                      <div className="items-start flex grow flex-col max-md:mt-10">
-                        <div className="text-white text-xl font-semibold leading-8 whitespace-nowrap bg-orange-400 w-[81px] max-w-full pl-7 pr-7 py-2.5 rounded-[50px] self-start max-md:px-5">
-                          02
-                        </div>
-                        <div className="text-cyan-900 text-3xl font-medium leading-[56.16px] self-stretch whitespace-nowrap mt-5">
-                          Global Reach
-                        </div>
+              <div className="mb-6">
+                <div className="flex">
+                  <div className="flex flex-col w-[48%] max-md:w-full max-md:ml-0">
+                    <div className=" flex flex-col max-md:mt-10">
+                      <div className="text-white text-xl font-semibold leading-8  bg-orange-400 w-[81px] text-center max-w-full pl-7 pr-7 py-1.5 rounded-[50px]  max-md:px-5">
+                        04
+                      </div>
+                      <div className="text-cyan-900 text-2xl font-medium leading-[36.16px]  mt-5">
+                        Secure Transactions
                       </div>
                     </div>
-                    <div className="flex flex-col items-stretch w-[52%] ml-5 max-md:w-full max-md:ml-0">
-                      <div className="text-cyan-900 text-base leading-8 w-[307px] max-w-[307px] my-auto max-md:mt-10">
-                        Hier connects you with people worldwide, making it easy
-                        to find the help you need, regardless of your location.
-                      </div>
+                  </div>
+                  <div className="flex flex-col items-stretch w-[52%] ml-5 max-md:w-full max-md:ml-0">
+                    <div className="text-cyan-900 text-base leading-8  my-auto max-md:mt-10">
+                      With a diverse community of users from different
+                      backgrounds and skills, Hier offers a wide range of
+                      services.
                     </div>
                   </div>
                 </div>
-                <div className="bg-black bg-opacity-10 self-stretch w-full h-px mt-6 max-md:max-w-full" />
+                <div className="bg-black bg-opacity-10  w-full h-px mt-5 max-md:max-w-full" />
               </div>
-              <div className="items-start self-stretch flex flex-col w-full mt-6 max-md:max-w-full">
-                <div className="self-stretch max-md:max-w-full">
-                  <div className="gap-5 flex max-md:flex-col max-md:items-stretch max-md:gap-0">
-                    <div className="flex flex-col items-stretch w-[48%] max-md:w-full max-md:ml-0">
-                      <div className="items-start flex grow flex-col max-md:mt-10">
-                        <div className="text-white text-xl font-semibold leading-8 whitespace-nowrap bg-orange-400 w-[81px] max-w-full pl-7 pr-7 py-2.5 rounded-[50px] self-start max-md:px-5">
-                          03
-                        </div>
-                        <div className="text-cyan-900 text-3xl font-medium leading-[56.16px] self-stretch whitespace-nowrap mt-5">
-                          Verified Profiles
-                        </div>
+              <div className="mb-6">
+                <div className="flex">
+                  <div className="flex flex-col w-[48%] max-md:w-full max-md:ml-0">
+                    <div className=" flex flex-col max-md:mt-10">
+                      <div className="text-white text-xl font-semibold leading-8  bg-orange-400 w-[81px] text-center max-w-full pl-7 pr-7 py-1.5 rounded-[50px]  max-md:px-5">
+                        04
                       </div>
-                    </div>
-                    <div className="flex flex-col items-stretch w-[52%] ml-5 max-md:w-full max-md:ml-0">
-                      <div className="text-cyan-900 text-base leading-8 w-[305px] max-w-[310px] mt-5 max-md:mt-10">
-                        We ensure the authenticity of users profiles, so you can
-                        trust the people you connect with.
+                      <div className="text-cyan-900 text-2xl font-medium leading-[36.16px]  mt-5">
+                        Secure Transactions
                       </div>
                     </div>
                   </div>
-                </div>{" "}
-                <div className="bg-black bg-opacity-10 self-stretch w-full h-px mt-6 max-md:max-w-full" />
-              </div>{" "}
-              <div className="items-start self-stretch flex grow flex-col w-full mt-6 max-md:max-w-full">
-                <div className="self-stretch max-md:max-w-full">
-                  <div className="gap-5 flex max-md:flex-col max-md:items-stretch max-md:gap-0">
-                    <div className="flex flex-col items-stretch w-[54%] max-md:w-full max-md:ml-0">
-                      <div className="items-start flex grow flex-col max-md:mt-5">
-                        <div className="text-white text-xl font-semibold leading-8 whitespace-nowrap bg-orange-400 w-[81px] max-w-full pl-7 pr-7 py-2.5 rounded-[50px] self-start max-md:px-5">
-                          04
-                        </div>{" "}
-                        <div className="text-cyan-900 text-3xl font-medium leading-[56.16px] whitespace-nowrap mt-5 self-start">
-                          Secure Transactions
-                        </div>
+                  <div className="flex flex-col items-stretch w-[52%] ml-5 max-md:w-full max-md:ml-0">
+                    <div className="text-cyan-900 text-base leading-8  my-auto max-md:mt-10">
+                      With a diverse community of users from different
+                      backgrounds and skills, Hier offers a wide range of
+                      services.
+                    </div>
+                  </div>
+                </div>
+                <div className="bg-black bg-opacity-10  w-full h-px mt-5 max-md:max-w-full" />
+              </div>
+              <div className="mb-0">
+                <div className="flex">
+                  <div className="flex flex-col w-[48%] max-md:w-full max-md:ml-0">
+                    <div className=" flex flex-col max-md:mt-10">
+                      <div className="text-white text-xl font-semibold leading-8  bg-orange-400 w-[81px] text-center max-w-full pl-7 pr-7 py-1.5 rounded-[50px]  max-md:px-5">
+                        04
                       </div>
-                    </div>{" "}
-                    <div className="flex flex-col items-stretch w-[46%] ml-5 max-md:w-full max-md:ml-0">
-                      <div className="text-cyan-900 text-base leading-8 w-[287px] max-w-[309px] my-auto max-md:mt-7">
-                        Our secure payment system guarantees peace of mind when
-                        hiring someone or offering your services.
+                      <div className="text-cyan-900 text-2xl font-medium leading-[36.16px]  mt-5">
+                        Secure Transactions
                       </div>
                     </div>
                   </div>
-                </div>{" "}
-                <div className="bg-black bg-opacity-10 self-stretch w-full h-px mt-6 max-md:max-w-full" />
+                  <div className="flex flex-col items-stretch w-[52%] ml-5 max-md:w-full max-md:ml-0">
+                    <div className="text-cyan-900 text-base leading-8  my-auto max-md:mt-10">
+                      With a diverse community of users from different
+                      backgrounds and skills, Hier offers a wide range of
+                      services.
+                    </div>
+                  </div>
+                </div>
+                <div className="bg-black bg-opacity-10  w-full h-px mt-5 max-md:max-w-full" />
               </div>
+
+              {/* fdfds */}
             </div>
           </div>
         </div>
