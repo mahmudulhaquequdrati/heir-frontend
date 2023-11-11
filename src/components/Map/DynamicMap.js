3c1c90116275710efa167a2e3671f759dbf289d8
kamruz-zzaman <kamruz.zaman8991@gmail.com>
2023-11-12 02:52:30 +0600

    marker show issue resolved

--- src/components/Map/DynamicMap.js
+++ src/components/Map/DynamicMap.js
@@ -2,7 +2,8 @@ import { useEffect } from 'react';
 import Leaflet from 'leaflet';
 import * as ReactLeaflet from 'react-leaflet';
 import 'leaflet/dist/leaflet.css';
-
+import "leaflet-defaulticon-compatibility";
+import "leaflet-defaulticon-compatibility/dist/leaflet-defaulticon-compatibility.css";
 import styles from './Map.module.css';
 
 const { MapContainer } = ReactLeaflet;
@@ -13,17 +14,6 @@ const Map = ({ children, className, width, height, ...rest }) => {
   if (className) {
     mapClassName = `${mapClassName} ${className}`;
   }
-
-  useEffect(() => {
-    (async function init() {
-      delete Leaflet.Icon.Default.prototype._getIconUrl;
-      Leaflet.Icon.Default.mergeOptions({
-        iconRetinaUrl: 'marker-icon-2x.png',
-        iconUrl: 'marker-icon.png',
-        shadowUrl: 'marker-shadow.png',
-      });
-    })();
-  }, []);
 
   return (
     <MapContainer className={mapClassName} {...rest}>
