79ee6843bae8cc4e0ccb0bfbf9df4c77ab6eb491
Shipon Hossen Raju <msshipon234@gmail.com>
2023-11-14 19:37:06 +0600

    add filter button

--- src/components/browseTask/TaskSection.js
+++ src/components/browseTask/TaskSection.js
@@ -1,3 +1,4 @@
+/* eslint-disable jsx-a11y/alt-text */
 "use client";
 import Link from "next/link";
 import { useState } from "react";
@@ -15,8 +16,8 @@ const TaskSection = () => {
           <MyMap />
         </div>
         <div>
-          <div className="mt-10">
-            <div className="relative">
+          <div className="mt-10 flex ml-3 justify-between min-w-full">
+            <div className="relative flex-1">
               <div className="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
                 <svg
                   className="w-4 h-4 text-gray-500 dark:text-gray-400"
@@ -49,6 +50,186 @@ const TaskSection = () => {
                 Search
               </button>
             </div>
+
+            {/* Filter */}
+            <button
+              className="w-max mx-2 sm:hidden block"
+              onClick={(e) => setOpenFilter(true)}
+            >
+              {/* <Image className="" src={filterIcons} width={50} height={50} /> */}
+              <span className="">
+                <svg
+                  className="w-8 h-8 font-bold"
+                  xmlns="http://www.w3.org/2000/svg"
+                  xmlnsXlink="http://www.w3.org/1999/xlink"
+                  // width={256}
+                  // height={256}
+                  viewBox="0 0 256 256"
+                  xmlSpace="preserve"
+                >
+                  <defs />
+                  <g
+                    style={{
+                      stroke: "none",
+                      strokeWidth: 0,
+                      strokeDasharray: "none",
+                      strokeLinecap: "butt",
+                      strokeLinejoin: "miter",
+                      strokeMiterlimit: 10,
+                      fill: "currentColor",
+                      fillRule: "nonzero",
+                      opacity: 1,
+                    }}
+                    transform="translate(1.4065934065934016 1.4065934065934016) scale(2.81 2.81)"
+                  >
+                    <path
+                      d="M 15.205 90 c -1.104 0 -2 -0.896 -2 -2 V 55.115 c 0 -1.104 0.896 -2 2 -2 s 2 0.896 2 2 V 88 C 17.205 89.104 16.31 90 15.205 90 z"
+                      style={{
+                        stroke: "none",
+                        strokeWidth: 1,
+                        strokeDasharray: "none",
+                        strokeLinecap: "butt",
+                        strokeLinejoin: "miter",
+                        strokeMiterlimit: 10,
+                        fill: "currentColor",
+                        fillRule: "nonzero",
+                        opacity: 1,
+                      }}
+                      transform=" matrix(1 0 0 1 0 0) "
+                      strokeLinecap="round"
+                    />
+                    <path
+                      d="M 74.795 59.357 c -1.104 0 -2 -0.896 -2 -2 V 2 c 0 -1.104 0.896 -2 2 -2 s 2 0.896 2 2 v 55.357 C 76.795 58.462 75.899 59.357 74.795 59.357 z"
+                      style={{
+                        stroke: "none",
+                        strokeWidth: 1,
+                        strokeDasharray: "none",
+                        strokeLinecap: "butt",
+                        strokeLinejoin: "miter",
+                        strokeMiterlimit: 10,
+                        fill: "currentColor",
+                        fillRule: "nonzero",
+                        opacity: 1,
+                      }}
+                      transform=" matrix(1 0 0 1 0 0) "
+                      strokeLinecap="round"
+                    />
+                    <path
+                      d="M 45 90 c -1.104 0 -2 -0.896 -2 -2 V 27.922 c 0 -1.104 0.896 -2 2 -2 s 2 0.896 2 2 V 88 C 47 89.104 46.104 90 45 90 z"
+                      style={{
+                        stroke: "none",
+                        strokeWidth: 1,
+                        strokeDasharray: "none",
+                        strokeLinecap: "butt",
+                        strokeLinejoin: "miter",
+                        strokeMiterlimit: 10,
+                        fill: "currentColor",
+                        fillRule: "nonzero",
+                        opacity: 1,
+                      }}
+                      transform=" matrix(1 0 0 1 0 0) "
+                      strokeLinecap="round"
+                    />
+                    <path
+                      d="M 45 29.922 c -5.464 0 -9.91 -4.445 -9.91 -9.91 s 4.445 -9.91 9.91 -9.91 c 5.465 0 9.91 4.445 9.91 9.91 S 50.465 29.922 45 29.922 z M 45 14.103 c -3.259 0 -5.91 2.651 -5.91 5.91 s 2.651 5.91 5.91 5.91 s 5.91 -2.651 5.91 -5.91 S 48.259 14.103 45 14.103 z"
+                      style={{
+                        stroke: "none",
+                        strokeWidth: 1,
+                        strokeDasharray: "none",
+                        strokeLinecap: "butt",
+                        strokeLinejoin: "miter",
+                        strokeMiterlimit: 10,
+                        fill: "currentColor",
+                        fillRule: "nonzero",
+                        opacity: 1,
+                      }}
+                      transform=" matrix(1 0 0 1 0 0) "
+                      strokeLinecap="round"
+                    />
+                    <path
+                      d="M 15.205 57.115 c -5.464 0 -9.91 -4.445 -9.91 -9.91 c 0 -5.464 4.445 -9.91 9.91 -9.91 s 9.91 4.445 9.91 9.91 C 25.115 52.67 20.669 57.115 15.205 57.115 z M 15.205 41.295 c -3.259 0 -5.91 2.651 -5.91 5.91 s 2.651 5.91 5.91 5.91 s 5.91 -2.651 5.91 -5.91 S 18.464 41.295 15.205 41.295 z"
+                      style={{
+                        stroke: "none",
+                        strokeWidth: 1,
+                        strokeDasharray: "none",
+                        strokeLinecap: "butt",
+                        strokeLinejoin: "miter",
+                        strokeMiterlimit: 10,
+                        fill: "currentColor",
+                        fillRule: "nonzero",
+                        opacity: 1,
+                      }}
+                      transform=" matrix(1 0 0 1 0 0) "
+                      strokeLinecap="round"
+                    />
+                    <path
+                      d="M 74.795 75.177 c -5.464 0 -9.909 -4.445 -9.909 -9.91 c 0 -5.464 4.445 -9.909 9.909 -9.909 c 5.465 0 9.91 4.445 9.91 9.909 C 84.705 70.731 80.26 75.177 74.795 75.177 z M 74.795 59.357 c -3.259 0 -5.909 2.65 -5.909 5.909 s 2.65 5.91 5.909 5.91 s 5.91 -2.651 5.91 -5.91 S 78.054 59.357 74.795 59.357 z"
+                      style={{
+                        stroke: "none",
+                        strokeWidth: 1,
+                        strokeDasharray: "none",
+                        strokeLinecap: "butt",
+                        strokeLinejoin: "miter",
+                        strokeMiterlimit: 10,
+                        fill: "currentColor",
+                        fillRule: "nonzero",
+                        opacity: 1,
+                      }}
+                      transform=" matrix(1 0 0 1 0 0) "
+                      strokeLinecap="round"
+                    />
+                    <path
+                      d="M 15.205 41.295 c -1.104 0 -2 -0.896 -2 -2 V 2 c 0 -1.104 0.896 -2 2 -2 s 2 0.896 2 2 v 37.295 C 17.205 40.4 16.31 41.295 15.205 41.295 z"
+                      style={{
+                        stroke: "none",
+                        strokeWidth: 1,
+                        strokeDasharray: "none",
+                        strokeLinecap: "butt",
+                        strokeLinejoin: "miter",
+                        strokeMiterlimit: 10,
+                        fill: "currentColor",
+                        fillRule: "nonzero",
+                        opacity: 1,
+                      }}
+                      transform=" matrix(1 0 0 1 0 0) "
+                      strokeLinecap="round"
+                    />
+                    <path
+                      d="M 45 14.103 c -1.104 0 -2 -0.896 -2 -2 V 2 c 0 -1.104 0.896 -2 2 -2 s 2 0.896 2 2 v 10.103 C 47 13.207 46.104 14.103 45 14.103 z"
+                      style={{
+                        stroke: "none",
+                        strokeWidth: 1,
+                        strokeDasharray: "none",
+                        strokeLinecap: "butt",
+                        strokeLinejoin: "miter",
+                        strokeMiterlimit: 10,
+                        fill: "currentColor",
+                        fillRule: "nonzero",
+                        opacity: 1,
+                      }}
+                      transform=" matrix(1 0 0 1 0 0) "
+                      strokeLinecap="round"
+                    />
+                    <path
+                      d="M 74.795 90 c -1.104 0 -2 -0.896 -2 -2 V 73.177 c 0 -1.104 0.896 -2 2 -2 s 2 0.896 2 2 V 88 C 76.795 89.104 75.899 90 74.795 90 z"
+                      style={{
+                        stroke: "none",
+                        strokeWidth: 1,
+                        strokeDasharray: "none",
+                        strokeLinecap: "butt",
+                        strokeLinejoin: "miter",
+                        strokeMiterlimit: 10,
+                        fill: "currentColor",
+                        fillRule: "nonzero",
+                        opacity: 1,
+                      }}
+                      transform=" matrix(1 0 0 1 0 0) "
+                      strokeLinecap="round"
+                    />
+                  </g>
+                </svg>
+              </span>
+            </button>
           </div>
         </div>
 
@@ -69,7 +250,7 @@ const TaskSection = () => {
       </div>
 
       <div
-        className={`absolute bg-white transition-all duration-500  [transition-timing-function: cubic-bezier(0, 0, 0, 0.8)] ${
+        className={`absolute bg-white transition-all duration-500 w-full [transition-timing-function: cubic-bezier(0, 0, 0, 0.8)] ${
           openFilter
             ? "visible md:invisible top-0 md:top-[100%] md:-z-[999px] z-[999px] inset-0"
             : "invisible top-[100%] left-0 right-0 -z-[999px] inset-auto"
